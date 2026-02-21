e9c592ef9fc2176d487e15ccfa7386e0411bf8d5
AinLume <space.voidar@gmail.com>
2026-02-21 16:34:18 +0300

    Initial commit

---
+++ backend/CafeCRM/src/main/java/org/example/cafecrm/CafeCrmApplication.java
@@ -0,0 +1,13 @@
+package org.example.cafecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeCrmApplication.class, args);
+    }
+
+}
---
+++ backend/CafeCRM/src/test/java/org/example/cafecrm/CafeCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cafecrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
