576b7d237add5075d1c0eb77a6277a2be7d07fba
AinLume <space.voidar@gmail.com>
2026-05-09 21:20:04 +0300

    feature: Добавлен метод для мапа списка в список респонсов

--- backend/CafeCRM/src/main/java/org/example/cafecrm/mapper/OrderItemMapper.java
+++ backend/CafeCRM/src/main/java/org/example/cafecrm/mapper/OrderItemMapper.java
@@ -1,5 +1,6 @@
 package org.example.cafecrm.mapper;
 
+import java.util.List;
 import org.example.cafecrm.domain.dto.order.OrderItemRequest;
 import org.example.cafecrm.domain.dto.order.OrderItemResponse;
 import org.example.cafecrm.domain.entity.OrderItem;
@@ -19,4 +20,9 @@ public interface OrderItemMapper {
     @Mapping(target = "order", ignore = true)
     @Mapping(target = "menuItem", ignore = true)
     OrderItem toEntity(OrderItemRequest request);
+
+    @Mapping(target = "orderItemId", source = "id")
+    @Mapping(target = "orderId", source = "order.id")
+    @Mapping(target = "destination", source = "destination")
+    List<OrderItemResponse> toResponseList(List<OrderItem> orderItems);
 }
