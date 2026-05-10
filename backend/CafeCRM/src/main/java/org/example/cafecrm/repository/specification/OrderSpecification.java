bae9bd2af7890d8c6c5c0ec89cb3ff2e4d03a104
AinLume <space.voidar@gmail.com>
2026-05-10 13:06:31 +0300

    refactor: Небольшие правки

--- backend/CafeCRM/src/main/java/org/example/cafecrm/repository/specification/OrderSpecification.java
+++ backend/CafeCRM/src/main/java/org/example/cafecrm/repository/specification/OrderSpecification.java
@@ -2,6 +2,7 @@ package org.example.cafecrm.repository.specification;
 
 import org.example.cafecrm.domain.entity.Order;
 import org.example.cafecrm.domain.values.OrderStatus;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.domain.Specification;
 
 public final class OrderSpecification {
@@ -9,19 +10,19 @@ public final class OrderSpecification {
     private OrderSpecification() {
     }
 
-    public static Specification<Order> hasStatus(OrderStatus status) {
+    public static Specification<@NotNull Order> hasStatus(OrderStatus status) {
         return (root, query, cb) -> status == null
                 ? cb.conjunction()
                 : cb.equal(root.get("status"), status);
     }
 
-    public static Specification<Order> hasClientId(Long clientId) {
+    public static Specification<@NotNull Order> hasClientId(Long clientId) {
         return (root, query, cb) -> clientId == null
                 ? cb.conjunction()
                 : cb.equal(root.get("client").get("id"), clientId);
     }
 
-    public static Specification<Order> hasTableId(Integer tableId) {
+    public static Specification<@NotNull Order> hasTableId(Integer tableId) {
         return (root, query, cb) -> tableId == null
                 ? cb.conjunction()
                 : cb.equal(root.get("table").get("id"), tableId);
